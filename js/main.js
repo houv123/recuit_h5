16dbe352417f3429777faad9c9a4b67f5899725b
weeky <houv123@sina.com>
2015-06-23 01:00:04 +0800

    recruit_h5

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    var pageSwiper = new Swiper('#page-swiper', {
+        direction: 'vertical',
+        initialSlide:3
+    });
+
+    var qstSwiper = new Swiper('#qst-swiper',{
+        direction: 'horizontal'
+    })
+});
